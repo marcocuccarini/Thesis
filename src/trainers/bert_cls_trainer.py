40e4eaca3daec6fd8a1a21543741fb1162d83226
marcocuccarini <59500902+marcocuccarini@users.noreply.github.com>
2023-03-03 15:45:08 +0100

    Update of metric_collection

--- src/trainers/bert_cls_trainer.py
+++ src/trainers/bert_cls_trainer.py
@@ -14,28 +14,30 @@ from src.utils.utils import plot_loss
 # This class is a wrapper for the training and testing of a Bert model for classification of dicscursive repertoires
 class BertClsTrainer():
     def __init__(self) -> None:
-        self.metric_collection = torchmetrics.MetricCollection({
+        import  torchmetrics
 
-            'accuracy_micro' : torchmetrics.Accuracy(num_classes=23, multiclass=True, average='micro'),
-            'accuracy_macro' : torchmetrics.Accuracy(num_classes=23, multiclass=True, average='macro'),
-            'accuracy_weighted' : torchmetrics.Accuracy(num_classes=23, multiclass=True, average='weighted'),
-            'accuracy_none' : torchmetrics.Accuracy(num_classes=23, multiclass=True, average='none'),
+# Metrics initialization
+        metric_collection = torchmetrics.MetricCollection({
+            'accuracy_micro' : torchmetrics.Accuracy(task='multiclass',num_classes=23, average='micro'),
+            'accuracy_macro' : torchmetrics.Accuracy(task='multiclass',num_classes=23, average='macro'),
+            'accuracy_weighted' : torchmetrics.Accuracy(task='multiclass',num_classes=23, average='weighted'),
+            'accuracy_none' : torchmetrics.Accuracy(task='multiclass',num_classes=23, average='none'),
 
-            'f1_micro' : torchmetrics.F1Score(num_classes=23, multiclass=True, average='micro'),
-            'f1_macro' : torchmetrics.F1Score(num_classes=23, multiclass=True, average='macro'),
-            'f1_weighted' : torchmetrics.F1Score(num_classes=23, multiclass=True, average='weighted'),
-            'f1_none' : torchmetrics.F1Score(num_classes=23, multiclass=True, average='none'),
+            'f1_micro' : torchmetrics.F1Score(task='multiclass',num_classes=23, average='micro'),
+            'f1_macro' : torchmetrics.F1Score(task='multiclass',num_classes=23, average='macro'),
+            'f1_weighted' : torchmetrics.F1Score(task='multiclass',num_classes=23, average='weighted'),
+            'f1_none' : torchmetrics.F1Score(task='multiclass',num_classes=23, average='none'),
 
-            'precision_micro' : torchmetrics.Precision(num_classes=23, multiclass=True, average='micro'),
-            'precision_macro' : torchmetrics.Precision(num_classes=23, multiclass=True, average='macro'),
-            'precision_weighted' : torchmetrics.Precision(num_classes=23, multiclass=True, average='weighted'),
-            'precision_none' : torchmetrics.Precision(num_classes=23, multiclass=True, average='none'),
+            'precision_micro' : torchmetrics.Precision(task='multiclass',num_classes=23, average='micro'),
+            'precision_macro' : torchmetrics.Precision(task='multiclass',num_classes=23, average='macro'),
+            'precision_weighted' : torchmetrics.Precision(task='multiclass',num_classes=23, average='weighted'),
+            'precision_none' : torchmetrics.Precision(task='multiclass',num_classes=23, average='none'),
 
-            'recall_micro' : torchmetrics.Recall(num_classes=23, multiclass=True, average='micro'),
-            'recall_macro' : torchmetrics.Recall(num_classes=23, multiclass=True, average='macro'),
-            'recall_weighted' : torchmetrics.Recall(num_classes=23, multiclass=True, average='weighted'),
-            'recall_none' : torchmetrics.Recall(num_classes=23, multiclass=True, average='none')
-        }) 
+            'recall_micro' : torchmetrics.Recall(num_classes=23, task='multiclass', average='micro'),
+            'recall_macro' : torchmetrics.Recall(num_classes=23, task='multiclass', average='macro'),
+            'recall_weighted' : torchmetrics.Recall(num_classes=23, task='multiclass', average='weighted'),
+            'recall_none' : torchmetrics.Recall(num_classes=23, task='multiclass', average='none')
+        })
         
     def fit(self, model, train_dataset, val_dataset, batch_size, lr, n_epochs, loss_fn):
         

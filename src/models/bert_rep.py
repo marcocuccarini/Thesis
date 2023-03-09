4112767a98c45155702376376d3f6002513c3870
marcocuccarini <59500902+marcocuccarini@users.noreply.github.com>
2023-03-09 10:51:16 +0100

    Fix embedding problem

--- src/models/bert_rep.py
+++ src/models/bert_rep.py
@@ -9,9 +9,9 @@ from src.datasets.hyperion_dataset import decode_labels
 # the model.
 class BertRep():
     def __init__(self, model_type):
-        self.tokenizer = AutoTokenizer.from_pretrained('MiBo/'+ model_type)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_type)
         self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-        self.model = AutoModelForSequenceClassification.from_pretrained('MiBo/RepML').to(self.device)
+        self.model = AutoModelForSequenceClassification.from_pretrained(model_type).to(self.device)
         self.model.eval()
     
     def predict(self, text:List[str]) -> List[str]:

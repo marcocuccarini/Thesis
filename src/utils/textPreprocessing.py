c60405ec605c0c5f4fdaa3fa3376346c21d7aa9b
marcocuccarini <59500902+marcocuccarini@users.noreply.github.com>
2023-04-19 11:23:36 +0200

    Create textPreprocessing.py

---
+++ src/utils/textPreprocessing.py
@@ -0,0 +1,88 @@
+import random
+import os
+
+import torch
+import numpy as np
+import datetime
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+
+punct = [',', '.', '"', ':', ')', '(', '-', '!', '?', '|', ';', "'", '$', '&', '/', '[', ']', '>', '%', '=', '#', '*', '+', '\\', '•',  '~', '@', '£', 
+ '·', '_', '{', '}', '©', '^', '®', '`',  '<', '→', '°', '€', '™', '›',  '♥', '←', '×', '§', '″', '′', 'Â', '█', '½', 'à', '…', 
+ '“', '★', '”', '–', '●', 'â', '►', '−', '¢', '²', '¬', '░', '¶', '↑', '±', '¿', '▾', '═', '¦', '║', '―', '¥', '▓', '—', '‹', '─', 
+ '▒', '：', '¼', '⊕', '▼', '▪', '†', '■', '’', '▀', '¨', '▄', '♫', '☆', 'é', '¯', '♦', '¤', '▲', 'è', '¸', '¾', 'Ã', '⋅', '‘', '∞', 
+ '∙', '）', '↓', '、', '│', '（', '»', '，', '♪', '╩', '╚', '³', '・', '╦', '╣', '╔', '╗', '▬', '❤', 'ï', 'Ø', '¹', '≤', '‡', '√', ]
+
+punct_mapping = {"‘": "'", "₹": "e", "´": "'", "°": "", "€": "e", "™": "tm", "√": " sqrt ", "×": "x", "²": "2", "—": "-", "–": "-", "’": "'", "_": "-",
+                 "`": "'", '“': '"', '”': '"', '“': '"', "£": "e", '∞': 'infinity', 'θ': 'theta', '÷': '/', 'α': 'alpha', '•': '.', 'à': 'a', '−': '-', 
+                 'β': 'beta', '∅': '', '³': '3', 'π': 'pi', '!':' '}
+
+def clean_text(text):
+    '''Clean emoji, Make text lowercase, remove text in square brackets,remove links,remove punctuation
+    and remove words containing numbers.'''
+    text = emoji.demojize(text)
+    text = re.sub(r'\:(.*?)\:','',text)
+    text = str(text).lower()    #Making Text Lowercase
+    text = re.sub('\[.*?\]', '', text)
+    #The next 2 lines remove html text
+    text = BeautifulSoup(text, 'lxml').get_text()
+    text = re.sub('https?://\S+|www\.\S+', '', text)
+    text = re.sub('<.*?>+', '', text)
+    text = re.sub('\n', '', text)
+    text = re.sub('\w*\d\w*', '', text)
+    # replacing everything with space except (a-z, A-Z, ".", "?", "!", ",", "'")
+    text = re.sub(r"[^a-zA-Z?.!,¿']+", " ", text)
+    return text
+
+def clean_contractions(text, mapping):
+    '''Clean contraction using contraction mapping'''    
+    specials = ["’", "‘", "´", "`"]
+    for s in specials:
+        text = text.replace(s, "'")
+    for word in mapping.keys():
+        if ""+word+"" in text:
+            text = text.replace(""+word+"", ""+mapping[word]+"")
+    #Remove Punctuations
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    # creating a space between a word and the punctuation following it
+    # eg: "he is a boy." => "he is a boy ."
+    text = re.sub(r"([?.!,¿])", r" \1 ", text)
+    text = re.sub(r'[" "]+', " ", text)
+    return text
+
+def clean_special_chars(text, punct, mapping):
+    '''Cleans special characters present(if any)'''   
+    for p in mapping:
+        text = text.replace(p, mapping[p])
+    
+    for p in punct:
+        text = text.replace(p, f' {p} ')
+    
+    specials = {'\u200b': ' ', '…': ' ... ', '\ufeff': '', 'करना': '', 'है': ''}  
+    for s in specials:
+        text = text.replace(s, specials[s])
+    
+    return text
+
+def correct_spelling(x, dic):
+    '''Corrects common spelling errors'''   
+    for word in dic.keys():
+        x = x.replace(word, dic[word])
+    return x
+
+def remove_space(text):
+    '''Removes awkward spaces'''   
+    #Removes awkward spaces 
+    text = text.strip()
+    text = text.split()
+    return " ".join(text)
+
+def text_preprocessing_pipeline(text):
+    '''Cleaning and parsing the text.'''
+    text = clean_text(text)
+    #text = clean_contractions(text, contractions_dict)
+    text = clean_special_chars(text, punct, punct_mapping)
+    text = remove_space(text)
+    return text

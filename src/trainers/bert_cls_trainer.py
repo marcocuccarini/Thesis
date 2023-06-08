15d2fff8b39a162bf731858aea2de280802a2cd6
marcocuccarini <59500902+marcocuccarini@users.noreply.github.com>
2023-06-08 14:36:08 +0200

    Update bert_cls_trainer.py

--- src/trainers/bert_cls_trainer.py
+++ src/trainers/bert_cls_trainer.py
@@ -333,7 +333,7 @@ class BertClsTrainer():
                 #loss = outputs[0]
                 logits = outputs[1]
                 
-                loss = loss_fn(logits.view(-1, n_label), b_labels.view(-1))
+                loss = loss_fn(logits.view(-1, n_label), b_labels)
                 
             # Accumulate the test loss.
             total_test_loss += loss.item()
